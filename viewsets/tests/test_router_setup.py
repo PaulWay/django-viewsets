c1c722a0db83080bf2c2db50b2671336b971186f
Paul Wayper <paulway@redhat.com>
2022-12-20 14:25:48 +1100

    Basic BaseRouter tests
    Signed-off-by: Paul Wayper <paulway@redhat.com>

--- viewsets/tests/test_router_setup.py
+++ viewsets/tests/test_router_setup.py
@@ -1,17 +1,23 @@
 from unittest import TestCase
 
 from viewsets.routers import BaseRouter, SimpleRouter
-from viewsets.viewsets import ViewSet, ModelViewSet, action
+from viewsets.viewsets import ViewSet, action
 
-class TestViewSet(ViewSet):
+class SimpleViewSet(ViewSet):
     template_path_base = ''
     # list and retrieve
 
 class RouterTest(TestCase):
     def test_basic_route(self):
         router = BaseRouter()
-        router.register(r'test', TestViewSet)
-        self.assertNotNone(router)
+        self.assertIsNotNone(router)
+        # Register with no basename is not implemented in BaseRouter
+        with self.assertRaises(NotImplementedError):
+            router.register(r'test', SimpleViewSet)
+        router.register(r'test', SimpleViewSet, basename='test')
+        # BaseRouter does not implement a way to get the URLs
+        with self.assertRaises(NotImplementedError):
+            router.urls
 
 # python -m unittest discover
 if __name__ == '__main__':
